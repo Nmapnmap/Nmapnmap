d510984b2845839279c8ce358965fe231888b05d
Nmapnmap <nmap@gmx.com>
2024-11-18 16:06:41 -0300

    Update and rename art to example.py

---
+++ example.py
@@ -0,0 +1,51 @@
+# This is an example of adding colors with color >
+
+mainArt = \
+'''
+root@kali:~# dmitry -winsepo example.txt
+Deepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Boa Sorte''
+
+HostIP:Inet-information for 2024 (31/12/2024)
+HostName:sortudo@megadavirada.com
+
+Gathered Inet-information for 2797 (16/11/2024)
+-----------------------------------------------
+
+Concurso 2797 (16/11/2024)  R$ 14.500.000,00
+
+01.03.15.25.45.52/2797      R$ 14.500.000,00
+
+Gathered Inet-information for 2796 (13/11/2024)
+-----------------------------------------------
+
+Concurso 2796 (13/11/2024)  R$ 8.500.000,00
+
+03.06.14.33.55.58/2796      R$ 8.500.000,00
+
+Gathered Inet-information for 03.06.14.33.55.58
+-----------------------------------------------
+
+Concurso 2795 (09/11/2024)  R$ 3.500.000,00
+
+13.16.33.43.46.55           R$ 3.500.000,00
+
+Gathered Inet-information for 2795 (09/11/2024)
+-----------------------------------------------
+
+Concurso 2794 (07/11/2024)  R$ 200.000.000,00
+
+03.09.14.20.28.52           R$ 200.000.000,00
+
+Gathered Inet-information for 2793 (05/11/2024)
+-----------------------------------------------
+
+Concurso 2793 (05/11/2024)  R$ 140.000.000,00
+
+07.09.25.37.57.59           R$ 140.000.000,00
+
+Gathered Inet-information   2793 (05/11/2024)
+-----------------------------------------------
+'''
